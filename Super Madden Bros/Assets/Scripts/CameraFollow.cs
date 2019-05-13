099c04d8003ec87872276f47ac5599956656aa2e
jsuhr2 <jsuhr2@binghamton.edu>
2019-05-13 18:04:27 -0400

    movement working, need jumping and collision detection next

--- Super Madden Bros/Assets/Scripts/CameraFollow.cs
+++ Super Madden Bros/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Transform target;
+    public Transform leftBound;
+    public Transform rightBound;
+
+    public float smoothDampTime = 0.15f;
+    private Vector3 smoothDampVelocity = Vector3.zero;
+
+    private float cameraWidth, cameraHeight, levelMinX, levelMaxX;
     // Start is called before the first frame update
     void Start()
     {
-        
+      cameraHeight = Camera.main.orthographicSize * 2;
+      cameraWidth = cameraHeight * Camera.main.aspect;
+
+      float leftBoundWidth = leftBound.GetComponentInChildren<SpriteRenderer> ().bounds.size.x / 2;
+      float rightBoundWidth = rightBound.GetComponentInChildren<SpriteRenderer> ().bounds.size.x / 2;
+
+      levelMinX = leftBound.position.x + leftBoundWidth + (cameraWidth / 2);
+      levelMaxX = rightBound.position.x - rightBoundWidth - (cameraWidth / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if(target){
+        float targetX = Mathf.Max(levelMinX, Mathf.Min(levelMaxX, target.position.x));
+        float x = Mathf.SmoothDamp(transform.position.x, targetX, ref smoothDampVelocity.x, smoothDampTime);
+        transform.position = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+      }
+
     }
 }
--- Super Madden Bros/Assets/Scripts/Player.cs
+++ Super Madden Bros/Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Vector2 velocity;
+    private bool walk, walkLeft, walkRight, jump;
+    public LayerMask wallMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,59 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckPlayerInput();
+        UpdatePlayerPosition();
+    }
+
+    void UpdatePlayerPosition()
+    {
+        Vector3 position = transform.localPosition;
+        Vector3 scale = transform.localScale;
+        if (walk)
+        {
+            if (walkLeft)
+            {
+                position.x -= velocity.x * Time.deltaTime;
+                scale.x = -1;
+            }
+            else if (walkRight)
+            {
+                position.x += velocity.x * Time.deltaTime;
+                scale.x = 1;
+            }
+        }
+
+        transform.localPosition = position;
+        transform.localScale = scale;
+    }
+
+    void CheckPlayerInput()
+    {
+        bool inputLeft = Input.GetKey(KeyCode.LeftArrow);
+        bool inputRight = Input.GetKey(KeyCode.RightArrow);
+        bool inputSpace = Input.GetKeyDown(KeyCode.Space);
+
+        walk = inputLeft || inputRight;
+        walkLeft = inputLeft && !inputRight;
+        walkRight = !inputLeft && inputRight;
+        jump = inputSpace;
+
+    }
+
+    Vector3 CheckWallRays(Vector3 position, float direction)
+    {
+        Vector2 originTop = new Vector2(position.x + direction * 0.4f, position.y + 1f - 0.2f);
+        Vector2 originMiddle = new Vector2(position.x + direction * 0.4f, position.y);
+        Vector2 originBottom = new Vector2(position.x + direction * 0.4f, position.y + 1f + 0.2f);
+        RaycastHit2D wallTop = Physics2D.Raycast(originTop, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
+        RaycastHit2D wallMiddle = Physics2D.Raycast(originMiddle, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
+        RaycastHit2D wallBottom = Physics2D.Raycast(originBottom, new Vector2(direction, 0), velocity.x * Time.deltaTime, wallMask);
+
+        if(wallTop.collider != null || wallMiddle.collider != null || wallBottom.collider != null)
+        {
+            position.x -= velocity.x * Time.deltaTime * direction;
+        }
+
+        return position;
     }
 }
